ef112dcb93a2e6985a8a695d2c060f874f9dce7b
Zachary Terrell <zachterrell57@ksu.edu>
2020-01-29 14:56:28 -0600

    AngryChicken done

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the angry chicken entree
+    /// </summary>
+    public class AngryChicken
+    {
+        /// <summary>
+        /// The price of the entree
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99;
+            }          
+        }
+
+        /// <summary>
+        /// The calories of the entree
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the entree should be served with a pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// If the entree should be served with bread
+        /// </summary>
+        public bool Bread { get; set; } = true; 
+
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                if (!Bread) { instructions.Add("hold bread"); }
+                if (!Pickle) { instructions.Add("hold pickle"); }
+                return instructions;
+            }
+        }
+
+
+
+
+
+
+    }
+}
