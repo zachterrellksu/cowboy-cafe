d46cb2e493ddebc55f92e93fa765cabe8cfe2db3
Zachary Terrell <zachterrell57@ksu.edu>
2020-02-03 15:16:41 -0600

    ChiliCheeseFries started

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class ChiliCheeseFries : Side
+    {
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 433;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Large:
+                        return 610;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+        public override double Price => throw new NotImplementedException();
+    }
+}
