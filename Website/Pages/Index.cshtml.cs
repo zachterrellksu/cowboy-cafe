0099d4f67af164235112c777113a04c9bf908ad1
Zachary Terrell <zachterrell57@ksu.edu>
2020-05-06 14:07:09 -0500

    Done with Website Milestone 3

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -24,36 +24,43 @@ namespace Website.Pages
         /// <summary>
         /// The items
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public IEnumerable<IOrderItem> Items { get; protected set; } = Menu.CompleteMenu();
 
         /// <summary>
         /// What the user is searching for 
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; }
 
         /// <summary>
         /// Different catagories of item
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public string[] Categories { get; set; }
-        
+
         /// <summary>
         /// Minimum amount of calories
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? CaloriesMin { get; set; }
 
         /// <summary>
         /// Maximum amount of calories
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? CaloriesMax { get; set; }
 
         /// <summary>
         /// Minimum Price
         /// </summary>
+        [BindProperty (SupportsGet = true)]
         public double? PriceMin { get; set; }
 
         /// <summary>
         /// Maximum amount of calories
         /// </summary>
+        [BindProperty (SupportsGet = true)]
         public double? PriceMax { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -68,20 +75,87 @@ namespace Website.Pages
             PriceMin = pmin;
             PriceMax = pmax;
             SearchTerms = Request.Query["SearchTerms"];
-            Categories = Request.Query["Categories"];
-            Items = Menu.Search(Items, SearchTerms);
-            Items = Menu.FilterByCategory(Items, Categories);
-            Items = Menu.FilterByCalories(Items, CaloriesMin, CaloriesMax);
-            Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
+            Categories = Request.Query["Categories"];           
+
+            Items = Menu.CompleteMenu();
+            if (SearchTerms != null)
+            {
+                Items = Items.Where(item => item.ToString() != null && 
+                                        item.ToString().Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));               
+            }
+
+            if (Categories != null && Categories.Length != 0)
+            {
+                Items = Items.Where(item => item != null && Categories.Contains(item.GetType().BaseType.Name));
+            }
+
+            if (CaloriesMin == null && CaloriesMax != null)
+            {
+                Items = Items.Where(item => item.Calories <= CaloriesMax);
+            }
+            if (CaloriesMax == null && CaloriesMin != null)
+            {
+                Items = Items.Where(item => item.Calories >= CaloriesMin);
+            }
+            if (CaloriesMax != null && CaloriesMin != null)
+            {
+
+                Items = Items.Where(item => item.Calories >= CaloriesMin && item.Calories <= CaloriesMax);
+            }
+
+            if (PriceMin == null && PriceMax != null)
+            {
+                Items = Items.Where(item => item.Price <= PriceMax);
+            }
+            if (PriceMax == null && PriceMin != null)
+            {
+                Items = Items.Where(item => item.Price >= PriceMin);
+            }
+            if (PriceMax != null && PriceMin != null)
+            {
+                Items = Items.Where(item => item.Price >= PriceMin && item.Price <= PriceMax);
+            }
         }
 
-        //public void OnPost()
-        //{
-        //    Items = Menu.Search(Items, SearchTerms);
-        //    Items = Menu.FilterByCategory(Items, Categories);
-        //    Items = Menu.FilterByCalories(Items, CaloriesMin, CaloriesMax);
-        //    Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
-        //}
-        
+        public void OnPost()
+        {    
+            if (SearchTerms != null)
+            {
+                Items = Items.Where(item => item.ToString() != null && 
+                                        item.ToString().Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            if (Categories != null && Categories.Length != 0)
+            {
+                Items = Items.Where(item => item != null && Categories.Contains(item.GetType().BaseType.Name));
+            }
+
+            if (CaloriesMin == null && CaloriesMax != null)
+            {
+                Items = Items.Where(item => item.Calories <= CaloriesMax);
+            }
+            if (CaloriesMax == null && CaloriesMin != null)
+            {
+                Items = Items.Where(item => item.Calories >= CaloriesMin);
+            }
+            if (CaloriesMax != null && CaloriesMin != null)
+            {
+
+                Items = Items.Where(item => item.Calories >= CaloriesMin && item.Calories <= CaloriesMax);
+            }
+
+            if (PriceMin == null && PriceMax != null)
+            {
+                Items = Items.Where(item => item.Price <= PriceMax);
+            }
+            if (PriceMax == null && PriceMin != null)
+            {
+                Items = Items.Where(item => item.Price >= PriceMin);
+            }
+            if (PriceMax != null && PriceMin != null)
+            {
+                Items = Items.Where(item => item.Price >= PriceMin && item.Price <= PriceMax);
+            }
+        }
     }
 }
